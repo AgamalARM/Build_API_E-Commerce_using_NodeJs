2233607f7e1599b55e15a443d8ef285e3c0fdfcb
AgamalARM <agamalarm@gmail.com>
2023-10-17 14:48:29 +0300

    create views 2

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,8 +3,8 @@ const Product = require("../models/product");
 const get_All_Products =  (req, res) => {
     Product.find().sort({ createdAt: -1 })
     .then(result => {
-    //   res.render('index', { blogs: result, title: 'All Products' });
-    res.send(result);
+      res.render('index', { blogs: result, title: 'All Products' });
+    // res.send(result);
     })
     .catch(err => {
       console.log(err);
